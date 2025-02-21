808ae5de27c8e697882cb10aab84a246292ef8fd
Jharna203 <jharnakhatun203@gmail.com>
2025-02-21 19:56:57 +0600

    Install and configure client file

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Login = () => {
+  return (
+    <div>
+      <img src="https://i.ibb.co.com/0pd1xqBd/logind.webp" alt="" />
+      <img src="https://i.ibb.co.com/bgsLnNcR/login.webp" alt="" />
+    </div>
+  )
+};
+
+export default Login;
