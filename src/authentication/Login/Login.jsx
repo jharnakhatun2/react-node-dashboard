aa45818e2c4c8737f5219c17a5d6d0102c420762
Jharna203 <jharnakhatun203@gmail.com>
2025-02-25 22:02:50 +0600

    Created Login Form

--- src/authentication/Login/Login.jsx
+++ src/authentication/Login/Login.jsx
@@ -1,59 +1,63 @@
 import React from "react"
+import { Link } from "react-router";
 
 const Login = () => {
   return (
-    <div>
-      <div>
-      <form className="space-y-5 mt-5">
-        <div>
-          <label>Image URL</label>
-          <input
-            type="text"
-            placeholder="Image Url"
-            name="image"
-            className="input-style"
-            required
-          />
+    <div className="w-full sm:w-md bg-[#f8f0e4] p-9 rounded-2xl shadow-2xl m-5">
+            <h1 className="uppercase text-2xl font-bold text-center text-gray-500">Bamboo Brush</h1>
+            <form className="space-y-5 my-5">
+                <div>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="title"
+                        className="input-style bg-[#f3e5d3] border-[#faeedc]"
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        className="input-style bg-[#f3e5d3] border-[#faeedc]"
+                        required
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="submit"
+                        value="Log In"
+                        className="bg-green-200 text-gray-700 font-medium md:max-w-full uppercase input-style cursor-pointer"
+                    />
+                </div>
+            </form>
+            <hr className="text-[#e6d4be] mb-5" />
+            <button
+                className="w-full font-bold shadow-sm rounded-lg py-3 bg-gray-500 text-white flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline cursor-pointer">
+                <div className="bg-white p-2 rounded-full">
+                    <svg className="w-4" viewBox="0 0 533.5 544.3">
+                        <path
+                            d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                            fill="#4285f4" />
+                        <path
+                            d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                            fill="#34a853" />
+                        <path
+                            d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                            fill="#fbbc04" />
+                        <path
+                            d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                            fill="#ea4335" />
+                    </svg>
+                </div>
+                <span className="ml-4">
+                    Sign Up with Google
+                </span>
+            </button>
+            <p className="text-center text-sm text-gray-500 pt-3">Have no Account ? <Link to='/register' className="text-green-400 font-bold">Create Account</Link></p>
         </div>
-        <div>
-          <label>Title</label>
-          <input
-            type="text"
-            placeholder="Type Title"
-            name="title"
-            className="input-style"
-            required
-          />
-        </div>
-        <div>
-          <label>Price</label>
-          <input
-            type="text"
-            placeholder="Type Price"
-            name="price"
-            className="input-style"
-            required
-          />
-        </div>
-        <div>
-          <label>Description</label>
-          <textarea
-            type="text"
-            placeholder="Type Description"
-            name="description"
-            className="input-style"
-          />
-        </div>
-        <div>
-          <input
-            type="submit"
-            value="Add New Product"
-            className="bg-green-300 text-gray-700 font-medium md:max-w-1/3 uppercase input-style"
-          />
-        </div>
-      </form>
-    </div>
-    </div>
   )
 };
 
